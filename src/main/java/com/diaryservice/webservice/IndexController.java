8aa9497ec36fc016f3d1d929f87e04685c462a75
kwondaeho <trewq231@naver.com>
2023-11-24 17:25:11 +0900

    Changed to return DTOs instead of entities

--- src/main/java/com/diaryservice/webservice/IndexController.java
+++ src/main/java/com/diaryservice/webservice/IndexController.java
@@ -4,6 +4,7 @@ package com.diaryservice.webservice;
 import com.diaryservice.webservice.config.auth.LoginUser;
 import com.diaryservice.webservice.config.auth.dto.SessionUser;
 import com.diaryservice.webservice.domain.event.Event;
+import com.diaryservice.webservice.dto.EventResponseDto;
 import com.diaryservice.webservice.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,8 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Controller
@@ -27,8 +30,16 @@ public class IndexController {
             model.addAttribute("userName", user.getName());
             Optional<List<Event>> allByUserId = eventService.findAllByUserId(user.getId());
             Optional<List<Event>> allByInvitedToUser = eventService.findAllByInvitedToUser(user.getId());
-            allByUserId.ifPresent(events -> model.addAttribute("events", events));
-            allByInvitedToUser.ifPresent(invitedEvent -> model.addAttribute("invitedEvent", invitedEvent));
+
+            List<EventResponseDto> createdDtos = allByUserId.map(events ->
+                    events.stream().map(Event::toDto).collect(Collectors.toList()))
+                    .orElse(Collections.emptyList());
+            List<EventResponseDto> invitedDtos = allByInvitedToUser.map(invitedEvents ->
+                    invitedEvents.stream().map(Event::toDto).collect(Collectors.toList()))
+                    .orElse(Collections.emptyList());
+
+            model.addAttribute("events", createdDtos);
+            model.addAttribute("invitedEvents", invitedDtos);
         }
 
         String errorMessage = (String) session.getAttribute("errorMessage");
